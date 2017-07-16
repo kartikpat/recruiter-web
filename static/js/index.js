3ca08f94ac29ab6409c309d34122a0e952cd9e05
patpatia <kartik@iimjobs.com>
2017-07-16 21:34:35 +0530

    changes in template

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,9 @@
 
+function openMenu() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
+}
