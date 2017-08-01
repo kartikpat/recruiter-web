8b3e0a71adc9e4c303637a7887ef09e3357e9e4c
5aurabh <saurabnanda@gmail.com>
2017-08-01 17:20:47 +0530

    chai.js set up for assertion

--- test/sample.js
+++ test/sample.js
@@ -1,8 +1,11 @@
 var assert = require('assert');
+var chai = require('chai');
+var expect = chai.expect;
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+      expect([1,2,3].indexOf(1)>-1).to.be.equal(true);
     });
   });
 });
