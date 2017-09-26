62035a3f9f5238c7d1890755d71078264bf1af42
5aurabh <saurabnanda@gmail.com>
2017-09-26 19:18:37 +0530

    fixed login

--- routes/home.js
+++ routes/home.js
@@ -23,7 +23,7 @@ module.exports = function(settings){
 		//bypassing the auth for development
     // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
     // you can do this however you want with whatever variables you set up
-    	if (req.session.authenticated)
+    	if (req.session.user)
         	return next();
     // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SOMEWHERE
 
