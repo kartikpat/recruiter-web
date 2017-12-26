4b6ea1e4f5dea4fc35e513caa1e7e49d4ba6186b
Raghav Kanwal <raghav.kanwal@gmail.com>
2017-12-26 19:00:42 +0530

    View Resume Page

--- static/js/candidate-apply-list.js
+++ static/js/candidate-apply-list.js
@@ -1,5 +1,26 @@
-console.log("Pippa");
-
 jQuery(".button-action-list").on("click", function() {
 	jQuery(this).toggleClass("inactive");
 })
+
+jQuery(".candidates-listing").on("click", ".candidate-item", function(e) {
+	if((!jQuery(e.target).parents(".candidate-item-section.profile-actions").length) && (!jQuery(e.target).parents(".candidate-item-section.image").length)) {
+		console.log("Open View Resume modal");
+
+		openViewResumeModal();
+	}
+});
+
+var openViewResumeModal = function() {
+	jQuery(".body-overlay").removeClass("hidden").addClass("veiled");
+	jQuery("body").addClass("posf");
+	jQuery(".view-resume-modal").removeClass("hidden");
+
+	jQuery("#tabbed-content").tabs();
+}
+
+jQuery(".body-overlay").on("click", function(e) {
+	if(jQuery(e.target).parents(".view-resume-modal").length) {
+		e.stopImmediatePropagation();
+	}
+	jQuery(".view-resume-modal").addClass("hidden");
+});
