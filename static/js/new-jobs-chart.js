48499d6b79095f2b32735b78309cbce3a37160ec
5aurabh <saurabnanda@gmail.com>
2017-12-27 12:58:11 +0530

    window resize chart done

--- static/js/new-jobs-chart.js
+++ static/js/new-jobs-chart.js
@@ -1,7 +1,18 @@
+var lockResize = false;
 $(document).ready(function(){
      google.charts.load('current', {
         'packages': ['corechart']
     }).then(function() {
         drawBarChartGraph(null, 'new-jobs-chart');
-    });
+    }); 
 })
+$(window).resize(function () {
+    if(lockResize)
+        return
+    lockResize = true;
+    setTimeout(function(){
+        drawBarChartGraph(null, 'new-jobs-chart');
+        console.log('calling timer')
+        lockResize = false;
+    }, 250)
+});
