4dee6e982d67b1cc661fcc510aa6c088b14b8956
patpatia <kartik@iimjobs.com>
2018-03-08 16:50:00 +0530

    resolved conflicts

--- static/js/Components/Header/index.js
+++ static/js/Components/Header/index.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-// $(document).ready(function(){
-// 	var headerDetails = Header();
-// 	headerDetails.init();
-// 	console.log(profile)
-// 	headerDetails.populateData(profile);
-//
-// })
-=======
+
 $(document).ready(function(){
 	var headerDetails = Header();
 	headerDetails.init();
-	console.log(profile)
-	headerDetails.populateData(profile);
+	
+	// headerDetails.populateData(profile);
 	headerDetails.myJobsView();
 	headerDetails.dashboardView();
 	headerDetails.searchView();
 })
->>>>>>> develop
