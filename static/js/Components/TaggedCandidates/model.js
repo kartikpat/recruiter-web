322315b80389e38aafdea77bcf64d265826f4df1
5aurabh <saurabnanda@gmail.com>
2018-03-20 18:41:25 +0530

    tagged candidate errors fixed

--- static/js/Components/TaggedCandidates/model.js
+++ static/js/Components/TaggedCandidates/model.js
@@ -10,6 +10,7 @@ function candidateList() {
         settings.candidateDownloadResumeButtonClass= ".candDownloadResume",
         settings.candidateProfessionalItemClass = '.candProfessionalItem',
         settings.candidateEducationItemClass = '.candEducationItem',
+        settings.candAppliedJobsClass= '.candAppliedJobs',
         settings.candTagItemClass= '.candTagItem',
         settings.tagOptionClass= '.tagOption',
         settings.tagId = -1,
@@ -127,14 +128,15 @@ function candidateList() {
        })
        item.eduList.html(eduStr)
        if(aData["applications"]) {
-           if(aData["applications"].length > 1) {
+           if(aData["applications"].length > 0) {
                item.candJobList.addClass("hidden")
                item.multipleJobListingText.text("Applied to "+aData["applications"].length+" jobs")
                var str = ""
                $.each(aData["applications"],function(index, application) {
                    console.log(application)
-                   var item =  $(''+settings.candAppliedJobsClass+'.prototype').clone().removeClass("prototype hidden");;
-                   item.html(application["title"] + " -<a href='/job/"+application["jobID"]+"/applications/"+application["id"]+"'> View Profile</a> ")
+                   var item =  $(''+settings.candAppliedJobsClass+'.prototype').clone().removeClass("prototype hidden");
+                   item.html(application["title"] + " -<a href='/job/"+application["jobID"]+"/applications/"+application["id"]+"'> View Profile</a> ");
+                   console.log(item)
                    str+=item[0].outerHTML
                })
                item.multipleCandJobListing.append(str);
