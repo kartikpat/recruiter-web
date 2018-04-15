e485148aad9f156c37947e9204167acf9ebe0b63
geetansh17 <geetanshuppal@gmail.com>
2018-04-16 00:14:04 +0530

    forgotpassword:loader

--- static/js/Components/ForgotPassword/index.js
+++ static/js/Components/ForgotPassword/index.js
@@ -4,15 +4,20 @@ $(document).ready(function() {
 	userForgotPswrd.submitHandler(function(e){
 		e.preventDefault()
 		if(userForgotPswrd.validate()){
+			$('#login').addClass('hidden');
+			$('.spinner').removeClass('hidden');
 			submitForgotPassword(userForgotPswrd.getData());
 		}
 	})
 
 	function onSuccessfulForgotPassword(topic, data) {
+		$('.button-wrapper').addClass('hidden');
 		userForgotPswrd.successMsg()
 	}
 
 	function onFailedForgotPassword(topic, data){
+		$('#login').removeClass('hidden');
+		$('.spinner').addClass('hidden');
 		userForgotPswrd.errorHandler(data);
 	}
 	var loginSuccessSubscription = pubsub.subscribe('successfulForgotPassword', onSuccessfulForgotPassword );
