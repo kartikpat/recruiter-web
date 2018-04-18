111a92386f4dfbbb906c43c70d025a9dfe3e3120
patpatia <kartik@iimjobs.com>
2018-04-18 12:10:10 +0530

    post job issues

--- static/js/Components/Header/model.js
+++ static/js/Components/Header/model.js
@@ -42,7 +42,7 @@ function Header(){
 			settings.CloseresumeModal=$('.modal_close');
 			settings.searchAccess = 1;
 
-			$(".recruiterLogout").click(function(){	
+			$(".recruiterLogout").click(function(){
 				document.cookie = "recruiter-access-token" + '=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;';
 				window.location.href = "/login"
 				return false
@@ -124,6 +124,7 @@ function Header(){
 
 	function resumeModal(profile){
 		settings.searchResume.on('click',function(){
+			console.log(profile.search)
 			if(profile.search && parseInt(profile.search) == 1) {
 				return window.location.href = 'https://search.iimjobs.com'
 			}
