72a04c5e70a14777079fcc97f277786c298e6488
geetansh17 <geetanshuppal@gmail.com>
2018-05-07 14:16:23 +0530

    ui-changers

---
+++ static/js/Tracker/tracker.js
@@ -0,0 +1,15 @@
+function sendPageView(title, path, location){
+	gtag('config', trackingId, {
+	  'page_title' : title,
+	  'page_path': path
+	  'page_location': location
+	});
+}
+var sendEvent = function(action, additionalObject){
+	// var sampleAdditinalObject = {
+	// 	event_category: 'some category',
+	// 	event_label: 'some label',
+	// 	value: 'any value'
+	// }
+	gtag('event', action, additionalObject);
+};
