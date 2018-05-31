2d00f0bd3e508702505499a0825f640c8eb3d022
5aurabh <saurabnanda@gmail.com>
2018-05-31 15:15:57 +0530

    gulp inter mediate

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,15 +4,15 @@
 
 var gulp = require("gulp");
 var gutil = require("gulp-util");
-var sass = require("gulp-sass");
+// var sass = require("gulp-sass");
 var del = require("del");	
 var notify = require("gulp-notify");
 var moment = require("moment");
-var uglifycss = require("gulp-uglifycss");
-var concat = require('gulp-concat');
+// var uglifycss = require("gulp-uglifycss");
+// var concat = require('gulp-concat');
 var staticMapper = require("./asset-mapper.json");
-var uglify = require('gulp-uglify');
-var gulpDebug = require('gulp-debug');
+// var uglify = require('gulp-uglify');
+// var gulpDebug = require('gulp-debug');
 
 // Use mocha for test driven development. But make this your last resort.
 // var mocha = require('./gulp-mocha')
@@ -25,53 +25,63 @@ function getAssetsArray(location){
 	return assets;
 }
 
-gulp.task('clean', function(){
+gulp.task('clean', function(done){
 	gutil.log('Cleaning build directory');
 	return del([
 			'static/build/css/**/*.css',
 			'static/build/js/**/*.js',
-		]);
+		], done);
 });
 
-gulp.task('build-css', function(){
-	gutil.log('building minified css');
-	for(var key in staticMapper){
-		gulp.src(getAssetsArray(staticMapper[key]["styles"]["debug"]))
-		.pipe(concat(staticMapper[key]["styles"]["prod"][0]))
-		.on('error', notify.onError("Error: <%= error.message %>"))
-		.pipe(uglifycss())
-		.on('error', notify.onError("Error: <%= error.message %>"))
-		.pipe(gulp.dest('.'))
-	    .pipe(notify('Concatenated stylesheets for ' + staticMapper[key]["styles"]["prod"][0] + ' (' + moment().format('MMM Do h:mm:ss A') + ')'))	
-	}
+gulp.task('default', gulp.series('clean'));
+
+
+/*****************************************/
+// gulp.task('build-css', function(){
+// 	gutil.log('building minified css');
+// 	for(var key in staticMapper){
+// 		gulp.src(getAssetsArray(staticMapper[key]["styles"]["debug"]))
+// 		.pipe(concat(staticMapper[key]["styles"]["prod"][0]))
+// 		.on('error', notify.onError("Error: <%= error.message %>"))
+// 		.pipe(uglifycss())
+// 		.on('error', notify.onError("Error: <%= error.message %>"))
+// 		.pipe(gulp.dest('.'))
+// 	    .pipe(notify('Concatenated stylesheets for ' + staticMapper[key]["styles"]["prod"][0] + ' (' + moment().format('MMM Do h:mm:ss A') + ')'))	
+// 	}
 	
-});
+// });
+/*****************************************/
 
-gulp.task('build-js', function(){
-	gutil.log('building minified js')
-	for(var key in staticMapper){
-		gulp.src(getAssetsArray(staticMapper[key]["scripts"]["debug"]))
-		.pipe(gulpDebug())
-        .on('error', notify.onError("Error: <%= error.message %>"))
-        .pipe(concat(staticMapper[key]["scripts"]["prod"][0]))
-        .on('error', notify.onError("Error: <%= error.message %>"))
-        .pipe(uglify())
-        .on('error', function(err){
-        	console.log(err);
-        })
-        //.on('error', notify.onError("Error: <%= error.message %>"))
-        .pipe(gulp.dest('.'))
-        .pipe(notify('Uglified JavaScript (' +staticMapper[key]["scripts"]["prod"][0]+ moment().format('MMM Do h:mm:ss A') + ')'))
-	}
-})
+/*****************************************/
 
+// gulp.task('build-js', function(){
+// 	gutil.log('building minified js')
+// 	for(var key in staticMapper){
+// 		gulp.src(getAssetsArray(staticMapper[key]["scripts"]["debug"]))
+// 		.pipe(gulpDebug())
+//         .on('error', notify.onError("Error: <%= error.message %>"))
+//         .pipe(concat(staticMapper[key]["scripts"]["prod"][0]))
+//         .on('error', notify.onError("Error: <%= error.message %>"))
+//         .pipe(uglify())
+//         .on('error', function(err){
+//         	console.log(err);
+//         })
+//         //.on('error', notify.onError("Error: <%= error.message %>"))
+//         .pipe(gulp.dest('.'))
+//         .pipe(notify('Uglified JavaScript (' +staticMapper[key]["scripts"]["prod"][0]+ moment().format('MMM Do h:mm:ss A') + ')'))
+// 	}
+// })
 
-gulp.task('build-sass',function(){
-	gulp.src('static/scss/**/*.scss')
-		.pipe(sass())
-		.on('error', notify.onError("Error: <%= error.message %>"))
-		.pipe(gulp.dest('static/css'))
-});
+/*****************************************/
+
+/*****************************************/
+// gulp.task('build-sass',function(){
+// 	gulp.src('static/scss/**/*.scss')
+// 		.pipe(sass())
+// 		.on('error', notify.onError("Error: <%= error.message %>"))
+// 		.pipe(gulp.dest('static/css'))
+// });
+/*****************************************/
 
 //For running test through mocha
 // gulp.task('run-test', function(){
@@ -80,20 +90,26 @@ gulp.task('build-sass',function(){
 //   //mocha({reporter: 'list'});
 // });
 
+/*****************************************/
 
-gulp.task('watch-test', function(){
-	gulp.watch('test/**.js', ['run-test']);
-})
+// gulp.task('watch-test', function(){
+// 	gulp.watch('test/**.js', ['run-test']);
+// })
+
+/*****************************************/
 
 // gulp.task('watch', ['clean','build-sass', 'build-css', 'build-js'], function(){
 // 	gulp.watch('static/scss/**/*.scss', ['build-sass']);
 // });
 
-gulp.task('watch', ['clean','build-sass', 'build-css'], function(){
-	gulp.watch('static/scss/**/*.scss', ['build-sass']);
-});
 
-gulp.task('build', ['clean', 'build-sass', 'build-css', 'build-js']);
+/*****************************************/
+// gulp.task('watch', ['clean','build-sass', 'build-css'], function(){
+// 	gulp.watch('static/scss/**/*.scss', ['build-sass']);
+// });
+
+// gulp.task('build', ['clean', 'build-sass', 'build-css', 'build-js']);
 
 
-gulp.task('default', ['watch']);
+// gulp.task('default', ['watch']);
+/*****************************************/
