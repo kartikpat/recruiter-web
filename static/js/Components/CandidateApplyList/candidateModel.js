42ed277d57a03e059fa01bb42f638bb3c89efbf0
geetansh17 <geetanshuppal@gmail.com>
2018-07-02 16:47:22 +0530

    facetoface:onhover

--- static/js/Components/CandidateApplyList/candidateModel.js
+++ static/js/Components/CandidateApplyList/candidateModel.js
@@ -871,7 +871,7 @@ function Candidate() {
                 settings.candidateDetailsModal.find(".candidateSaveModal").html("<span class='icon'><i class='icon-star'></i></span>Saved for Later");
             }
             else {
-                settings.candidateDetailsModal.find(".candidateSaveModal").html("<span class='icon'><i class='icon-star_later'></i></span>Save for Later");
+                settings.candidateDetailsModal.find(".candidateSaveModal").html("<span class='icon'><i class='icon-star_later'></i></span><span class='underline'>Save for Later</span>");
             }
             if(newStatus == settings.candidateDetailsModal.find(".candidateRejectModal").attr("data-action")) {
                 settings.candidateDetailsModal.find(".candidateRejectModal").text("Rejected").addClass("act-rej")
